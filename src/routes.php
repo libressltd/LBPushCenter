fb6f252ed0f8c7b8baf4f7d7b5f507b250b24c75
iConcept <iconcept@MacBook-Pro.local>
2017-02-21 19:10:33 +0700

    quick fix

--- src/routes.php
+++ src/routes.php
@@ -13,6 +13,6 @@ Route::group(['prefix' => 'lbpushcenter', 'namespace' => 'libressltd\lbpushcente
 	});
 
 	Route::group(['prefix' => 'ajax', 'middleware' => ['web']], function () {
-		Route::resource("device", "Ajax\Push_deviceController");]
+		Route::resource("device", "Ajax\Push_deviceController");
 	});
 });
