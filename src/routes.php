120785d2165516fc94cbdc56a440fe645ced020e
iConcept <iconcept@MacBook-Pro.local>
2017-04-16 11:24:36 +0700

    Fix

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,8 @@
 <?php
 
-Route::group(['prefix' => 'lbpushcenter', 'namespace' => 'libressltd\lbpushcenter\controllers', "middleware" => "auth"], function (){
+Route::group(['prefix' => 'lbpushcenter', 'namespace' => 'libressltd\lbpushcenter\controllers'], function (){
 
-	Route::group(['middleware' => ['web']], function () {
+	Route::group(['middleware' => ['web', "auth"]], function () {
 		Route::resource("application", "Push_applicationController");
 		Route::resource("application_type", "Push_applicationTypeController");
 		Route::resource("device", "Push_deviceController");
@@ -11,15 +11,15 @@ Route::group(['prefix' => 'lbpushcenter', 'namespace' => 'libressltd\lbpushcente
 		Route::resource("dashboard", "Push_dashboardController");
 	});
 
-	Route::group(['prefix' => 'ajax', 'middleware' => ['web']], function () {
+	Route::group(['prefix' => 'api', 'middleware' => 'api'], function () {
+		Route::resource("device", "Service\Push_deviceController");
+		Route::post("device/{device_id}/clear_badge", "Service\Push_deviceController@postClearBadge");
+	});
+
+	Route::group(['prefix' => 'ajax', 'middleware' => ['web', "auth"]], function () {
 		Route::resource("device", "Ajax\Push_deviceController");
 		Route::resource("worker", "Ajax\Push_workerController");
 		Route::resource("application", "Ajax\Push_applicationController");
 		Route::resource("notification", "Ajax\Push_notificationController");
 	});
-});
-
-Route::group(['prefix' => 'lbpushcenter/api', 'middleware' => 'api', 'namespace' => 'libressltd\lbpushcenter\controllers\Service'], function () {
-	Route::resource("device", "Push_deviceController");
-	Route::post("device/{device_id}/clear_badge", "Push_deviceController@postClearBadge");
 });
