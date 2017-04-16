4b15b6b3997976a521e6eea14ad171ebbd40e297
iConcept <iconcept@MacBook-Pro.local>
2017-04-16 10:57:03 +0700

    Fix

--- src/routes.php
+++ src/routes.php
@@ -11,15 +11,15 @@ Route::group(['prefix' => 'lbpushcenter', 'namespace' => 'libressltd\lbpushcente
 		Route::resource("dashboard", "Push_dashboardController");
 	});
 
-	Route::group(['prefix' => 'api', 'middleware' => 'api'], function () {
-		Route::resource("device", "Service\Push_deviceController");
-		Route::post("device/{device_id}/clear_badge", "Service\Push_deviceController@postClearBadge");
-	});
-
 	Route::group(['prefix' => 'ajax', 'middleware' => ['web']], function () {
 		Route::resource("device", "Ajax\Push_deviceController");
 		Route::resource("worker", "Ajax\Push_workerController");
 		Route::resource("application", "Ajax\Push_applicationController");
 		Route::resource("notification", "Ajax\Push_notificationController");
 	});
+});
+
+Route::group(['prefix' => 'api', 'middleware' => 'api', 'namespace' => 'libressltd\lbpushcenter\controllers\Service'], function () {
+	Route::resource("device", "Push_deviceController");
+	Route::post("device/{device_id}/clear_badge", "Push_deviceController@postClearBadge");
 });
